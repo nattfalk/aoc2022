2add1ea8d3ec4d31ea756ca1542e7140640c7e53
Michael Nattfalk <michael.nattfalk@consid.se>
2022-12-01 08:06:08 +0100

    Day 1

---
+++ day1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.IO;
+using System.Linq;
+
+day1();
+day2();
+
+void day1()
+{
+    var lines = File.ReadAllText(@"input1.txt");
+
+    var groups = lines.Split(Environment.NewLine + Environment.NewLine);
+    List<int> maxList = new();
+    foreach(var group in groups)
+    {
+        maxList.Add(group.Split("\n").Select(x => int.Parse(x)).Sum());
+    }
+
+    Console.WriteLine(maxList.Max());
+}
+
+void day2()
+{
+    var lines = File.ReadAllText(@"input1.txt");
+
+    var groups = lines.Split(Environment.NewLine + Environment.NewLine);
+    List<int> maxList = new();
+    foreach(var group in groups)
+    {
+        maxList.Add(group.Split("\n").Select(x => int.Parse(x)).Sum());
+    }
+
+    Console.WriteLine(maxList.OrderDescending().Take(3).Sum());
+}

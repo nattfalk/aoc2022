44df77b855bddb3fb9b59fdb565c1844b1a27c75
nattfalk <michaelnattfalk@gmail.com>
2022-12-06 20:50:56 +0100

    Day 6 - F# solution

---
+++ day6/fsharp/Program.fs
@@ -0,0 +1,23 @@
+﻿let input = System.IO.File.ReadAllText @"..\input.txt"
+
+// let input = "bvwbjplbgvbhsrlpgdmjqwftvncz" // 5
+// let input = "nppdvjthqldpwncqszvftbrmjlhg" // 6
+// let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" // 10
+// let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" // 11
+
+let findMarker x =
+    let distinctChars s =
+        s
+        |> Seq.distinct
+        |> Seq.length
+
+    let findMatch = 
+        input
+        |> Seq.windowed x
+        |> Seq.map distinctChars
+        |> Seq.findIndex (fun elm -> elm = x)
+
+    (findMatch) + x
+
+printfn "%d" (findMarker 4)
+printfn "%d" (findMarker 14)

26dac1cb756f47c2bd9e151c81392a7b70b9ad22
nattfalk <michaelnattfalk@gmail.com>
2022-12-06 21:20:26 +0100

    Day 1 - F# solution

---
+++ day1/fsharp/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+
+let input = System.IO.File.ReadAllText @"..\input1.txt"
+// let input = @"1000
+// 2000
+// 3000
+
+// 4000
+
+// 5000
+// 6000
+
+// 7000
+// 8000
+// 9000
+
+// 10000"
+
+let calc (inp:string) =
+    let sumGroup (grp:string) =
+        grp.Split Environment.NewLine
+        |> Seq.map int
+        |> Seq.sum
+    
+    let groupTotals =
+        inp.Split $"{Environment.NewLine}{Environment.NewLine}"
+        |> Seq.map sumGroup
+
+    printfn "Part 1 : %i" (groupTotals |> Seq.max)
+
+    let part2 =
+        groupTotals
+        |> Seq.sortDescending
+        |> Seq.take 3
+        |> Seq.sum
+    printfn "Part 2 : %i" part2
+
+calc input
